b3f75b7503114e2c96f1c7158ffc3d5a2178a495
adcr <caique.macedo@targetdata.com.br>
2021-03-17 18:39:11 -0300

    Alterando para o banco H2 e escluindo a camada de exceptions que nao era utilizada agora.

--- src/main/java/br/com/fiap/arremate/mscomprador/exceptions/CompradorNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package br.com.fiap.arremate.mscomprador.exceptions;
-
-public class CompradorNotFoundException extends RuntimeException {
-
-  public CompradorNotFoundException() {
-    super("Não foi possível encontrar um comprador ");
-  }
-}
